437535829a239d38f6f8a0f7b8dcbe7c39b56689
Jerry Marino <i@jerrymarino.com>
2018-05-19 16:27:07 -0700

    Import LogParser

--- Package.swift
+++ Package.swift
@@ -8,13 +8,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Carthage/Commandant", from: "0.13.0"),
+        .package(url: "https://github.com/jerrymarino/SwiftCompilationDatabase.git", .revision( "810f040e97ebe97210d64a644d8d386e35a6358d")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SPMVim",
-            dependencies: ["Commandant"]),
+            dependencies: ["Commandant", "LogParser"]),
         .testTarget(
             name: "SPMVimTests",
             dependencies: ["SPMVim"]),
