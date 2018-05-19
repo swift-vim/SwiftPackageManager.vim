32696b0c81d8a6fe5630599f8667d2da5587fcbe
Jerry Marino <i@jerrymarino.com>
2018-05-19 13:32:02 -0700

    Add commadant

--- Package.swift
+++ Package.swift
@@ -6,21 +6,20 @@ import PackageDescription
 let package = Package(
     name: "SPMVim",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SPMVim",
             targets: ["SPMVim"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Carthage/Commandant", from: "0.13.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SPMVim",
-            dependencies: []),
+            dependencies: ["Commandant"]),
         .testTarget(
             name: "SPMVimTests",
             dependencies: ["SPMVim"]),
