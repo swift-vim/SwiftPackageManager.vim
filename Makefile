88ff90fc64de9e60a6312d1d0ae8d9964b7cbe21
Jerry Marino <i@jerrymarino.com>
2018-05-19 16:48:00 -0700

    Dump compile commnads

--- Makefile
+++ Makefile
@@ -1,4 +1,49 @@
+# Wrap SPM build system
+# We call the program this.
+PRODUCT=spm-vim
+LAST_LOG=.build/$(CONFIG)/last.log
+
+.PHONY: install
+install: CONFIG=release
+install: build-impl
+	@echo "Installing to /usr/local/bin/$(PRODUCT)"
+	ditto .build/$(CONFIG)/$(PRODUCT) /usr/local/bin/$(PRODUCT)
+
+.PHONY: build-impl
+build-impl:
+	@echo "Building.."
+	@swift build -c $(CONFIG) $(SWIFT_OPTS) | tee -a $(LAST_LOG)
+	@mv .build/$(CONFIG)/SPMVim .build/$(CONFIG)/$(PRODUCT)
+
+build: CONFIG=debug
+build: build-impl
+
+.PHONY: release
+release: CONFIG=release
+release: build-impl
+
+# Running: Pipe the parseable output example to the program
 .PHONY: run
-run:
-	swift build
-	./.build/debug/SPMVim log
+run: CONFIG=debug
+run: build-impl
+	cat parseable-output-example.txt | .build/$(CONFIG)/$(PRODUCT)
+
+.PHONY: run_compile_commands
+run_compile_commands: CONFIG=debug
+run_compile_commands: build-impl
+	.build/$(CONFIG)/$(PRODUCT) compile_commands parseable-output-example.txt 
+
+.PHONY: clean
+clean:
+	rm -rf .build/debug/*
+	rm -rf .build/release/*
+
+# Build compile_commands.json
+# Unfortunately, we need to clean.
+# Use the last installed product incase we messed something up during
+# coding.
+compile_commands.json: SWIFT_OPTS=-Xswiftc -parseable-output
+compile_commands.json: CONFIG=debug
+compile_commands.json: clean build-impl
+	cat $(LAST_LOG) | /usr/local/bin/$(PRODUCT) 
+
--- Sources/SPMVim/main.swift
+++ Sources/SPMVim/main.swift
@@ -1,20 +1,49 @@
 import Commandant
 import Result
+import LogParser
+import Foundation
+
+func getPath(path: String, relativeTo: String) -> String {
+    return path.hasSuffix("/") ?
+        path : relativeTo + "/" + path
+}
 
 enum BasicError : Error {
   case none
 }
 
 struct LogCommand: CommandProtocol {
-  typealias Options = LogOptions
+    typealias Options = LogOptions
 
-  let verb = "log"
-  let function = "Reads the log"
+    let verb = "log"
+    let function = "Reads the log"
 
-  func run(_ options: Options) -> Result<(), BasicError> {
-    print("Run")
-    return .success(())
-  }
+    func run(_ options: Options) -> Result<(), BasicError> {
+        let assumedDir = FileManager.default.currentDirectoryPath
+        var logPath: String?
+        if let logName = options.logName { 
+            logPath =  getPath(path: logName, relativeTo: assumedDir)
+        }
+
+        if fcntl(FileHandle.standardInput.fileDescriptor, F_GETFL) != 0 
+          && logPath == nil {
+            print("""
+            Usage:
+                cat parseable-build.log | spm-vim log
+                or
+                spm-vim log parseable-build.log
+                To produce a parseable run swift with -parseable-output
+            """)
+            // FIXME
+            return .success(())
+        }
+        let messages = LogParser.readMessages(logPath: logPath)
+        let db = LogParser.renderCompileCommands(messages: messages, dir:
+            assumedDir)
+        let outFile = URL(fileURLWithPath: assumedDir + "/compile_commands.json")
+        try? db.write(to: outFile, atomically: false, encoding: .utf8)
+        return .success(())
+    }
 }
 
 struct LogOptions: OptionsProtocol {
@@ -30,8 +59,54 @@ struct LogOptions: OptionsProtocol {
   }
 }
 
+struct CompileCommandsCommand: CommandProtocol {
+    typealias Options = LogOptions
+    let verb = "compile_commands"
+    let function = "Generates compile_commands.json"
+
+    func run(_ options: Options) -> Result<(), BasicError> {
+        let assumedDir = FileManager.default.currentDirectoryPath
+        var logPath: String?
+        if let logName = options.logName { 
+            logPath =  getPath(path: logName, relativeTo: assumedDir)
+        }
+        if fcntl(FileHandle.standardInput.fileDescriptor, F_GETFL) != 0 
+          && logPath == nil {
+            print("""
+            Usage:
+                cat parseable-build.log | spm-vim log
+                or
+                spm-vim log parseable-build.log
+                To produce a parseable run swift with -parseable-output
+            """)
+            // FIXME
+            return .success(())
+        }
+        let messages = LogParser.readMessages(logPath: logPath)
+        let db = LogParser.renderCompileCommands(messages: messages, dir:
+            assumedDir)
+        let outFile = URL(fileURLWithPath: assumedDir + "/compile_commands.json")
+        try? db.write(to: outFile, atomically: false, encoding: .utf8)
+        return .success(())
+    }
+}
+
+struct CompileCommandsOptions: OptionsProtocol {
+  let logName: String?
+
+  static func create(_ logName: String?) -> CompileCommandsOptions {
+    return CompileCommandsOptions(logName: logName)
+  }
+
+  static func evaluate(_ m: CommandMode) -> Result<CompileCommandsOptions, CommandantError<BasicError>> {
+    return create
+      <*> m <| Argument(usage: "the log to read")
+  }
+}
+
 let commands = CommandRegistry<BasicError>()
 commands.register(LogCommand())
+commands.register(CompileCommandsCommand())
 
 // Commandant Boilderplate
 let arguments = CommandLine.arguments
