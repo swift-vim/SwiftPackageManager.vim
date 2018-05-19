5e22ef0092e9ae698ee0a2a453c3187bd4267172
Jerry Marino <i@jerrymarino.com>
2018-05-19 13:55:58 -0700

    Add makefile

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: run
+run:
+	swift build
+	./.build/debug/SPMVim
--- Sources/SPMVim/main.swift
+++ Sources/SPMVim/main.swift
@@ -6,33 +6,54 @@ enum BasicError : Error {
 }
 
 struct LogCommand: CommandProtocol {
-	typealias Options = LogOptions
+  typealias Options = LogOptions
 
-	let verb = "log"
-	let function = "Reads the log"
+  let verb = "log"
+  let function = "Reads the log"
 
-	func run(_ options: Options) -> Result<(), BasicError> {
-		// Use the parsed options to do something interesting here.
-		return .success(())
-	}
+  func run(_ options: Options) -> Result<(), BasicError> {
+    print("Run")
+    return .success(())
+  }
 }
 
 struct LogOptions: OptionsProtocol {
-	let lines: Int
-	let verbose: Bool
-	let logName: String
+  let lines: Int
+  let verbose: Bool
+  let logName: String
 
-	static func create(_ lines: Int) -> (Bool) -> (String) -> LogOptions {
-		return { verbose in { logName in LogOptions(lines: lines, verbose: verbose, logName: logName) } }
-	}
+  static func create(_ lines: Int) -> (Bool) -> (String) -> LogOptions {
+    return { verbose in { logName in LogOptions(lines: lines, verbose: verbose, logName: logName) } }
+  }
 
-	static func evaluate(_ m: CommandMode) -> Result<LogOptions, CommandantError<BasicError>> {
-		return create
-			<*> m <| Option(key: "lines", defaultValue: 0, usage: "the number of lines to read from the logs")
-			<*> m <| Option(key: "verbose", defaultValue: false, usage: "show verbose output")
-			<*> m <| Argument(usage: "the log to read")
-	}
+  static func evaluate(_ m: CommandMode) -> Result<LogOptions, CommandantError<BasicError>> {
+    return create
+      <*> m <| Option(key: "lines", defaultValue: 0, usage: "the number of lines to read from the logs")
+      <*> m <| Option(key: "verbose", defaultValue: false, usage: "show verbose output")
+      <*> m <| Argument(usage: "the log to read")
+  }
 }
 
 let commands = CommandRegistry<BasicError>()
 commands.register(LogCommand())
+
+var arguments = CommandLine.arguments
+
+// Remove the executable name.
+assert(!arguments.isEmpty)
+arguments.remove(at: 0)
+
+print("Run")
+if let verb = arguments.first {
+  // Remove the command name.
+  arguments.remove(at: 0)
+
+  if let result = commands.run(command: verb, arguments: arguments) {
+    // Handle success or failure.
+  } else {
+    // Unrecognized command.
+  }
+} else {
+  // No command given.
+}
+
